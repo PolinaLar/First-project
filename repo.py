2ba5e7ef19fd1065150d34c3d9e3a92cb8bc6596
Yulia <iu.azarova@sch-int.ru>
2025-09-26 14:12:30 +0300

    repo commit 2

--- repo.py
+++ repo.py
@@ -1,1 +1,17 @@
-import db.py
+import db
+import sqlite3
+
+connect = sqlite3.connect("homework.db") 
+cursor = connect.cursor()
+
+def add_book(): 
+    cursor.execute(""" update books
+                   set total +=1, free += 1
+                   if exists (title, author)
+                   else  set total = 1, free = 1""")
+    
+def delete_book():
+    cursor.execute(""" delete from books
+                   where id = ?
+                   if not exists (pr.loans, pr.holds)""", int(input()))
+
