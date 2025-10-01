f09466a598c6f0c5870dc7aafa646b57e5b397e8
YuliaAzarova <67875047+YuliaAzarova@users.noreply.github.com>
2025-10-01 14:16:33 +0300

    repo.py finish

--- repo.py
+++ repo.py
@@ -1,20 +1,25 @@
-import db
 import sqlite3
 
-connect = sqlite3.connect("homework.db") 
+connect = sqlite3.connect("project.db") 
 cursor = connect.cursor()
 
 def add_book(title, author, genre, n): 
     cursor.execute(""" update books
-                   if exists (title, author) set total += ?, free += ?
-                   else  set total = 1, free = 1, title = ?, author = ?, genre = ?""", n, 
+                   set total += ?, free += ?
+                   where title is not null and author is not null
+                   set total = 1, free = 1, title = ?, author = ?, genre = ?
+                   where title is null and author is null """, n, 
                    n, title, author, genre)
     
-def delete_book():
+def delete_book(title, author):
     cursor.execute(""" delete from books
-                   where id = ?
-                   if not exists (pr.loans, pr.holds)""", int(input()))
+                   where (title = ?, author = ?)
+                   if not exists (pr.loans, pr.holds)""", title, author)
 
 def add_reader(pr, full_name, phone, age):
     cursor.executemany(""" insert into readers (pr,full_name, phone, age)
         values (?,?,?)""", pr,full_name, phone, age)
+
+def delete_reader(pr):
+    cursor.execute(""" delete from readers
+                   where pr = ? """, pr)
