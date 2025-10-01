4c709fc25bc67a81ef05646c2e32c21e16c71eee
YuliaAzarova <67875047+YuliaAzarova@users.noreply.github.com>
2025-10-01 14:20:57 +0300

    repo.py finish 2

--- repo.py
+++ repo.py
@@ -10,16 +10,20 @@ def add_book(title, author, genre, n):
                    set total = 1, free = 1, title = ?, author = ?, genre = ?
                    where title is null and author is null """, n, 
                    n, title, author, genre)
+    connect.commit()
     
 def delete_book(title, author):
     cursor.execute(""" delete from books
                    where (title = ?, author = ?)
                    if not exists (pr.loans, pr.holds)""", title, author)
+    connect.commit()
 
 def add_reader(pr, full_name, phone, age):
     cursor.executemany(""" insert into readers (pr,full_name, phone, age)
         values (?,?,?)""", pr,full_name, phone, age)
+    connect.commit()
 
 def delete_reader(pr):
     cursor.execute(""" delete from readers
                    where pr = ? """, pr)
+    connect.commit()
