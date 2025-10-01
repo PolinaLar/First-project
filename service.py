16e34817386791758af99c55cf1088f323658f22
Polina <test@email.ru>
2025-10-01 15:36:37 +0300

    service_delete

--- service.py
+++ service.py
@@ -9,7 +9,7 @@ def book(pr, title, author):
                    where title = ? and author = ?""",(title, author))
     
     book_1 = cursor.fetchone()
-    title_id = book_1[0]
+    book_id = book_1[0]
     book_free = book_1[5]
     cursor.execute("""select count(*) from holds
                    where pr == ?
@@ -17,12 +17,32 @@ def book(pr, title, author):
     count = cursor.fetchone()[0]
     if (book_free > 0 and count < 5):
         cursor.execute("""insert into holds (pr,book_id,date)
-        values (?,?,?)""", (pr,title_id,date))
+        values (?,?,?)""", (pr,book_id,date))
 
     cursor.execute(""" update books
         set free = ?
-        where id = ?""", (count-1,title_id))
+        where id = ?""", (count-1,book_id))
     
     connect.commit()
 print(datetime.now().strftime("%d/%m/%y"))
-#book("ПЛ691122", "Евгений Онегин", "Пушкин", "11/12/25")
+#book("ПЛ691122", "Евгений Онегин", "Пушкин")
+
+
+def delete_hold(pr, title, author):
+    cursor.execute("""select * from books
+                   where title = ? and author = ?""",(title, author))
+    book = cursor.fetchone()
+    book_id = book[0]
+    book_free = book[5]
+
+    cursor.execute(""" delete from holds
+    where pr = ? and book_id = ?
+    """, (pr,book_id))
+    
+    cursor.execute(""" update books
+        set free = ?
+        where id = ?""", (book_free+1,book_id))
+    
+    connect.commit()
+
+#delete_hold("ПЛ691122", "Евгений Онегин", "Пушкин")
