bc724bc161a03a39ab109c13c1e80a64858e6379
Polina <test@email.ru>
2025-10-02 14:01:15 +0300

    held

--- service.py
+++ service.py
@@ -92,3 +92,9 @@ def return_book(pr, title, author):
         set free += 1
         where book_id = ?""", (row[0]))
     connect.commit()
+
+
+def held_books(pr):
+    cursor.execute("""select title, author, date_hold, expires_at from holds
+    where pr = ?""", (pr))
+    return cursor.fetchall()
